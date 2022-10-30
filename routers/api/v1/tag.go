724fdcbed72e67239e2b1d308bd42251d66118f2
熠丶 <490493499@qq.com>
2022-10-30 22:38:44 +0800

    编写标签列表的路由逻辑

--- routers/api/v1/tag.go
+++ routers/api/v1/tag.go
@@ -1,9 +1,42 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+	"go-gin-example/models"
+	"go-gin-example/pkg/e"
+	"go-gin-example/pkg/setting"
+	"go-gin-example/pkg/util"
+	"net/http"
+)
 
 // GetTags 获取多个文章标签
 func GetTags(c *gin.Context) {
+	name := c.Query("name")
+
+	maps := make(map[string]interface{})
+	data := make(map[string]interface{})
+
+	if name != "" {
+		maps["name"] = name
+	}
+
+	var state int = -1
+	if arg := c.Query("state"); arg != "" {
+		state = com.StrTo(arg).MustInt()
+		maps["state"] = state
+	}
+
+	code := e.SUCCESS
+
+	data["lists"] = models.GetTags(util.GetPage(c), setting.PageSize, maps)
+	data["total"] = models.GetTagTotal(maps)
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg":  e.GetMsg(code),
+		"data": data,
+	})
 }
 
 // AddTag 新增文章标签
